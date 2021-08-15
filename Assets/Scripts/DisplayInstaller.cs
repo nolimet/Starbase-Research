597988b2098239eafcb06d85ec30877685ef6292
Jesse Stam <nolimet@stamdata.nl>
2021-08-15 23:39:02 +0200

    Added DisplayInstaller

---
+++ Assets/Scripts/DisplayInstaller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(fileName = "Display Installer", menuName = "Installers/Display Installer")]
+public class DisplayInstaller : ScriptableObjectInstaller
+{
+    [SerializeField]
+    private MaterialItem materialItemPrefab;
+
+    [SerializeField]
+    private ResearchItem researchItemPrefab;
+
+    public override void InstallBindings()
+    {
+        Container.Bind<ResearchDataService>().ToSelf().AsCached().IfNotBound();
+
+        Container.BindFactory<MaterialItem, MaterialItem.Factory>().FromComponentInNewPrefab(materialItemPrefab);
+        Container.BindFactory<ResearchItem, ResearchItem.Factory>().FromComponentInNewPrefab(researchItemPrefab);
+    }
+}
