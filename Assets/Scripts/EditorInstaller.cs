ecb4aa932852bce323d43725f2229b0bf25dd1eb
Jesse Stam <nolimet@stamdata.nl>
2021-08-15 20:56:39 +0200

    added editor installer script

---
+++ Assets/Scripts/EditorInstaller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(fileName = "Editor Installer", menuName = "Installers/Editor Installer")]
+public class EditorInstaller : ScriptableObjectInstaller
+{
+    [SerializeField]
+    private MaterialDataEditor materialDataEditorPrefab;
+
+    [SerializeField]
+    private MaterialIconsConfiguration materialIconsConfiguration;
+
+    public override void InstallBindings()
+    {
+        Container.Bind<ResearchDataService>().ToSelf().AsSingle();
+        Container.BindInterfacesAndSelfTo<TabActionService>().AsSingle();
+
+        Container.BindInstance(materialIconsConfiguration).AsSingle();
+
+        Container.BindFactory<MaterialDataEditor, MaterialDataEditor.Factory>().FromComponentInNewPrefab(materialDataEditorPrefab);
+    }
+}
