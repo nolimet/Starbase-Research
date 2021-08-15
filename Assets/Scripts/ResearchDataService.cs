380c410fc5472245077c32acb42baa398aad43d1
Jesse Stam <nolimet@stamdata.nl>
2021-08-15 23:39:50 +0200

    Fixed events and added a auto load

--- Assets/Scripts/ResearchDataService.cs
+++ Assets/Scripts/ResearchDataService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Networking;
 
 public class ResearchDataService
@@ -10,12 +11,21 @@ public class ResearchDataService
     private const string filename = "researchdata.json";
     private readonly string filePath = Path.Combine(Application.streamingAssetsPath, filename);
 
-    private readonly List<ResearchObject> researchObjects;
+    public event UnityAction DataChanged;
+
+    private readonly List<ResearchObject> researchObjects = new List<ResearchObject>();
+
+    public ResearchDataService()
+    {
+        LoadData();
+    }
+
     public IReadOnlyList<ResearchObject> ResearchObjects => researchObjects;
 
     public void AddNewResearchObject(ResearchObject researchObject)
     {
         researchObjects.Add(researchObject);
+        DataChanged?.Invoke();
     }
 
     public async void LoadData()
@@ -32,6 +42,7 @@ public class ResearchDataService
                 var researchData = JsonConvert.DeserializeObject<ReasearchData>(webrequest.downloadHandler.text);
                 researchObjects.Clear();
                 researchObjects.AddRange(researchData.objects);
+                DataChanged?.Invoke();
             }
         }
     }
