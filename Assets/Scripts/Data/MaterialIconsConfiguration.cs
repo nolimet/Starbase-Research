d1591826895647abeb101eb207e9cc40d95d855a
Jesse Stam <nolimet@stamdata.nl>
2021-08-15 20:53:49 +0200

    added data classes and mapped out all the resources

---
+++ Assets/Scripts/Data/MaterialIconsConfiguration.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Material Icons Configuration", menuName = "Configuration/MaterialIconConfiguration")]
+public class MaterialIconsConfiguration : ScriptableObject
+{
+    [SerializeField]
+    private Material[] materials;
+
+    private Dictionary<ResourcesType, Sprite> lookupTable;
+
+    public Sprite GetIconForMaterial(ResourcesType type)
+    {
+        if (lookupTable == null)
+        {
+            lookupTable = materials.ToDictionary(x => x.material, x => x.icon);
+        }
+
+        return lookupTable[type];
+    }
+
+    private void UpdateList()
+    {
+        if (Application.isPlaying)
+
+            return;
+
+        var resourceTypes = Enum.GetValues(typeof(ResourcesType)) as ResourcesType[];
+        if (materials == null)
+        {
+            materials = new Material[0];
+        }
+        materials = materials.Concat
+           (
+               resourceTypes
+               .Where
+               (
+                   t =>
+                       t != ResourcesType.None && !materials.Any(
+                           m =>
+                               m.material == t
+                           )
+               )
+               .Select(x => new Material(x))
+               .OrderBy(x => x.material)
+           ).ToArray();
+    }
+
+    [Serializable]
+    private class Material
+    {
+        public Sprite icon;
+        public ResourcesType material;
+
+        public Material(ResourcesType material)
+        {
+            this.material = material;
+        }
+
+        public Material(Sprite icon, ResourcesType material)
+        {
+            this.icon = icon;
+            this.material = material;
+        }
+    }
+}
---
+++ Assets/Scripts/Data/ResourcesTypes.cs
@@ -0,0 +1,34 @@
+using System;
+
+[Serializable]
+public enum ResourcesType
+{
+    None = 0,
+    Aegisium = 1,
+    Ajatite = 2,
+    Arkanium = 3,
+    Bastium = 4,
+    Charodium = 5,
+    Corazium = 6,
+    Daltium = 7,
+    Exorium = 8,
+    Haderite = 9,
+    Ice = 10,
+    Ilmatrium = 11,
+    Karnite = 12,
+    Kutonium = 13,
+    Lukium = 14,
+    Merkerium = 15,
+    Naflite = 16,
+    Nhurgite = 17,
+    Oninum = 18,
+    Surtrite = 19,
+    Talkite = 20,
+    Targium = 21,
+    Tengium = 22,
+    Ukonium = 23,
+    Valkite = 24,
+    Vokarium = 25,
+    Xhalium = 26,
+    Ymrium = 28,
+}
