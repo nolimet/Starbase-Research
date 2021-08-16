af4a5cdda48026b139602ca10e7f20b050098c54
Jesse Stam <nolimet@stamdata.nl>
2021-08-16 13:52:58 +0200

    added a sorting functino to the datadisplay

--- Assets/Scripts/DataDisplay/DataDisplay.cs
+++ Assets/Scripts/DataDisplay/DataDisplay.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Zenject;
 
@@ -15,6 +17,36 @@ public class DataDisplay : MonoBehaviour
 
     private readonly List<ResearchItem> researchItems = new List<ResearchItem>();
 
+    public void OrderByTotalKV(bool reversed) => OrderBy(reversed, x => x.Searchdata.totalKV);
+
+    public void OrderByCubeEfficiency(bool reversed) => OrderBy(reversed, x => x.Searchdata.cubeEfficiency);
+
+    public void OrderByPowerEfficiency(bool reversed) => OrderBy(reversed, x => x.Searchdata.powerEfficiency);
+
+    public void OrderByGearEfficiency(bool reversed) => OrderBy(reversed, x => x.Searchdata.gearEfficiency);
+
+    public void OrderByShieldEfficiency(bool reversed) => OrderBy(reversed, x => x.Searchdata.shieldEfficiency);
+
+    private void OrderBy<TKey>(bool reversed, Func<ResearchItem, TKey> keySelector)
+    {
+        List<ResearchItem> items;
+        if (reversed)
+        {
+            items = researchItems.OrderBy(keySelector).Reverse().ToList();
+        }
+        else
+        {
+            items = researchItems.OrderBy(keySelector).ToList();
+        }
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            ResearchItem item = items[i];
+            item.transform.SetSiblingIndex(i);
+            item.SetBackgroundEnabled(i % 2 == 0);
+        }
+    }
+
     private void Awake()
     {
         researchDataService.DataChanged += ResearchDataService_DataChanged;
@@ -30,7 +62,9 @@ public class DataDisplay : MonoBehaviour
 
         foreach (var researchItem in researchDataService.ResearchObjects)
         {
-            researchItems.Add(researchItemFactory.Create(researchItemContainer, researchItem));
+            var newItem = researchItemFactory.Create(researchItemContainer, researchItem);
+            newItem.SetBackgroundEnabled(researchItems.Count % 2 == 0);
+            researchItems.Add(newItem);
         }
     }
 }
